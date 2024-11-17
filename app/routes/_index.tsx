e4a038dd14f383d2568490af712fc00009707b61
bornayo7 <yashbaruah@hotmail.com>
2024-11-17 05:15:59 -0600

    Adding contactus and aboutus

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,15 @@
-// ~/routes/index.tsx (Adjust the path if different)
-import type { LoaderFunction, MetaFunction } from '@remix-run/node';
-import { Suspense, useState } from 'react';
-import { isDesktop } from 'react-device-detect';
-import MapContainer from '~/components/map/MapContainer';
-import SidebarContainerDesktop from '~/components/sidebar/SidebarDesktopContainer';
-import SidebarContainerMobile from '~/components/sidebar/SidebarMobileContainer';
-import { useWindowSize } from '~/utils/hooks';
-import { Color } from '~/utils/types';
-import { MarkerLocations } from '~/utils/marker_locations';
-import ColorSwitcher from '~/components/ColorSwitcher';
-import DropUpButton from '~/components/DropUpButton'; // Import the DropUpButton
+// ~/routes/index.tsx
+import type { LoaderFunction, MetaFunction } from '@remix-run/node'
+import { Suspense, useState } from 'react'
+import { isDesktop } from 'react-device-detect'
+import MapContainer from '~/components/map/MapContainer'
+import SidebarContainerDesktop from '~/components/sidebar/SidebarDesktopContainer'
+import SidebarContainerMobile from '~/components/sidebar/SidebarMobileContainer'
+import { useWindowSize } from '~/utils/hooks'
+import { Color } from '~/utils/types'
+import { MarkerLocations } from '~/utils/marker_locations'
+import ColorSwitcher from '~/components/ColorSwitcher'
+import DropUpButton from '~/components/DropUpButton' // Import the DropUpButton
 
 export const meta: MetaFunction = () => {
   return [
@@ -18,15 +18,15 @@ export const meta: MetaFunction = () => {
       name: 'Comet Park',
       content: 'Get help finding a spot in the UTD parking lots!',
     },
-  ];
-};
+  ]
+}
 
 export default function Index() {
-  const [windowWidth, windowHeight] = useWindowSize();
-  const largeViewport = windowWidth > 768;
+  const [windowWidth, windowHeight] = useWindowSize()
+  const largeViewport = windowWidth > 768
 
-  const [switcherActive, setSwitcherActive] = useState<Boolean>(false);
-  const [color, setColor] = useState<Color>('green');
+  const [switcherActive, setSwitcherActive] = useState<Boolean>(false)
+  const [color, setColor] = useState<Color>('green')
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -46,14 +46,14 @@ export default function Index() {
       ) : (
         <SidebarContainerMobile />
       )}
-      <DropUpButton /> {/* Add the DropUpButton here */}
+      <DropUpButton pageType="home" /> {/* Add the DropUpButton here */}
     </Suspense>
-  );
+  )
 }
 
 export const loader: LoaderFunction = async ({ request, params, context }) => {
-  const slug = params.index;
+  const slug = params.index
 
-  console.log(params);
-  return null;
-};
+  console.log(params)
+  return null
+}
